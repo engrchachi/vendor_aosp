83b27ad14f6db0886c1bbb85e18d4e5a0063fff0
SuperDroidBond <superdroidbond@yahoo.com>
2018-05-31 18:06:47 +0530

    vendor: Add Neoneon Font
    * Thanks @Bootleggers ROM
    
    Change-Id: Ida45b9d2172fb5470b60bef5d6bf2c2e366fc83e
    Signed-off-by: Sipun Ku Mahanta <sipunkumar85@gmail.com>

--- config/fonts.mk
+++ config/fonts.mk
@@ -6,4 +6,5 @@ PRODUCT_COPY_FILES += \
     vendor/aosp/prebuilt/fonts/gobold/GoboldBold-Italic.ttf:system/fonts/GoboldBold-Italic.ttf \
     vendor/aosp/prebuilt/fonts/gobold/GoboldThinLight.ttf:system/fonts/GoboldThinLight.ttf \
     vendor/aosp/prebuilt/fonts/gobold/GoboldThinLight-Italic.ttf:system/fonts/GoboldThinLight-Italic.ttf \
+    vendor/aosp/prebuilt/fonts/neoneon/neoneon.ttf:system/fonts/Neoneon-Regular.ttf \
     vendor/aosp/prebuilt/fonts/Shamshung/Shamshung.ttf:system/fonts/Shamshung.ttf
