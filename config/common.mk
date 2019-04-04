SuperiorOS/android_vendor_superior

Vendor: Add samsung font
Signed-off-by: Ded_Boi <dhanush911gtr@gmail.com>
Signed-off-by: Sipun Ku Mahanta <sipunkumar85@gmail.com>

config/common.mk
@@ -175,6 +175,9 @@ $(call inherit-product, vendor/superior/config/telephony.mk)
 # Packages
 include vendor/superior/config/packages.mk
 
+# Fonts
+include vendor/superior/config/fonts.mk
+
 # Superior_props
 $(call inherit-product, vendor/superior/config/superior_props.mk)
 

engrchachi/vendor_aosp

Change-Id: Ibca19da4cedbdb5f89e733cf97fc73588106ac3c

config/common.mk
@@ -161,4 +161,7 @@ include vendor/pixelstyle/config.mk
 # Themes
 include vendor/themes/config.mk
 
+# Fonts
+include vendor/aosp/config/fonts.mk
+
 -include $(WORKSPACE)/build_env/image-auto-bits.mk