723f08185afe03b83f9507a8381e49fe6d6f226b
Alif Fathur <46101724+darkthur@users.noreply.github.com>
2019-04-08 21:24:29 +0700

    Fix Changeloh

--- tools/changelog.sh
+++ tools/changelog.sh
@@ -1,9 +1,6 @@
-#!/bin/sh
-
 export Changelog=${ANDROID_BUILD_TOP}/vendor/aosp/tools/Changelog.md
 
 echo "Generating changelogs..."
 
 cp $Changelog $OUT/system/etc/
 cp $Changelog $OUT/
-rm -rf $Changelog
