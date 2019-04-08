11e32dce9594ac6867990edbd4ccc387d3492cc9
Alif Fathur <46101724+darkthur@users.noreply.github.com>
2019-04-08 21:35:44 +0700

    Change to TXt format

--- tools/changelog.sh
+++ tools/changelog.sh
@@ -2,5 +2,5 @@ export Changelog=${ANDROID_BUILD_TOP}/vendor/aosp/tools/Changelog.md
 
 echo "Generating changelogs..."
 
-cp $Changelog $OUT/system/etc/
-cp $Changelog $OUT/
+cp $Changelog $OUT/system/etc/Changelog.txt
+cp $Changelog $OUT/Changelog.txt
