e722b01f1edb632a7a0ca3dbe62c84e2fc849fb6
Alif Fathur <46101724+darkthur@users.noreply.github.com>
2019-04-13 23:43:03 +0700

    Back to the original one

--- config/ota.mk
+++ config/ota.mk
@@ -8,11 +8,11 @@ CUSTOM_OTA_VERSION_CODE := pie
 endif
 
 CUSTOM_PROPERTIES += \
-    com.aosplime.limeota.version_code=$(CUSTOM_OTA_VERSION_CODE) \
+    org.pixelexperience.version_code=$(CUSTOM_OTA_VERSION_CODE) \
     sys.ota.disable_uncrypt=1
 
  PRODUCT_PACKAGES += \
-    LimeOTA
+    Updates
 
 PRODUCT_COPY_FILES += \
     vendor/aosp/config/permissions/com.aosplime.ota.xml:system/etc/permissions/com.aosplime.ota.xml
